c36a2374a39ad2d1da66a2f697b075651c005388
Khoa Nguyen <khoa8a522@gmail.com>
2023-07-01 23:40:41 +0700

    axios fetch products

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import data from './data';
+import React, { useEffect, useState } from 'react';
+// import data from './data';
 import classNames from 'classnames/bind';
 import styles from './Home.module.scss';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const cx = classNames.bind(styles);
 export default function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <h1 className={cx('title')}>Sản phẩm đang bán</h1>
       <div className={cx('products')}>
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className={cx('product')} key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img
